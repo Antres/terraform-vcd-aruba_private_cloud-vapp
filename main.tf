7e84ab9e20b4b936f4e81e9ed7ab69c5f15e99c5
Alberto Clemente <albertoclemente1983@gmail.com>
2020-05-10 14:05:59 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,13 +14,3 @@ resource "vcd_vapp" "vapp" {
   name                      = var.name
 }
 
-resource "vcd_vapp_org_network" "net" {
-  for_each                  = {for network in var.networks:  network.network.name => network}
-
-  org                       = var.region.vdc.org
-  vdc                       = var.region.vdc.name
-
-  vapp_name                 = vcd_vapp.vapp.name
-
-  org_network_name  = each.value.network.name
-}
